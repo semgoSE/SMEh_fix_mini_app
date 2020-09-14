3a77de4013404dc18ace742f829b8b17e4b5bf67
semgoSE <semgoSE@gmail.com>
2020-09-14 19:53:52 +0300

    Updates

---
+++ precache-manifest.a2b60a40f53fc8e36464faa257e695f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1597d3aa9243c3b0b6afde66a88e8c2c",
+    "url": "/SMEh_fix_mini_app/index.html"
+  },
+  {
+    "revision": "f0d797fd9df6f4201de8",
+    "url": "/SMEh_fix_mini_app/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "f0d797fd9df6f4201de8",
+    "url": "/SMEh_fix_mini_app/static/js/2.916dada6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SMEh_fix_mini_app/static/js/2.916dada6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c92c16bd862c3895aa7",
+    "url": "/SMEh_fix_mini_app/static/js/main.a10e778e.chunk.js"
+  },
+  {
+    "revision": "6e181416cc23ae45705e",
+    "url": "/SMEh_fix_mini_app/static/js/runtime-main.177c2f41.js"
+  }
+]);
